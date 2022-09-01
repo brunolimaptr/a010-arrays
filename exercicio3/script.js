70194a02d4bea988ed68835a394af17570932f14
brunolimaptr <brunolima.ptr@gmail.com>
2022-09-01 13:18:51 -0300

    projeto

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -7,8 +7,24 @@ const arraynumerocopia = arraynumero.slice()
 const arraystringcopia = arraystring.slice()
 const arraymistocopia = arraymisto.slice()
 
+const numero = arraynumero
+const numerocopia = arraynumerocopia
+const string = arraystring
+const stringcopia = arraystringcopia
+const misto = arraymisto
+const mistocopia = arraymistocopia
+
 arraynumerocopia.push(13)
 
-console.log(arraynumero[0,1,2,3],arraynumerocopia[0,1,2,3,4])
+console.log(numero)
+console.log(numerocopia)
 
+arraystringcopia.pop()
 
+console.log(string)
+console.log(stringcopia)
+
+arraymistocopia.splice(1,1)
+
+console.log(misto)
+console.log(mistocopia)
